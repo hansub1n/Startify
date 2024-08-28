d0900dc806fdaf53ed4d804b30c476ca3bb50651
Subin, Han <ella0411@naver.com>
2024-08-28 21:35:58 +0900

    add: 수파베이스 연결

---
+++ src/supabaseClient.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+// 1) project url
+const SUPABASE_PROJECT_URL = import.meta.env.VITE_SUPABASE_PROJECT_URL;
+
+// 2) anon key
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
+export default supabase;
