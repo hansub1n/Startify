cc30f923a9375be35826a7a59e2748361bdfc1b2
t2023-m0125 <t2023-m0125@t2023-m0125ui-MacBookAir.local>
2024-08-30 15:46:09 +0900

    fix:유튜브추가

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { styled } from "styled-components";
+import { getYoutubeKey } from "../utils";
 
 const PostTitle = styled.div``;
 const SongTitle = styled.div``;
@@ -9,6 +10,7 @@ const Name = styled.div``;
 const Genre = styled.div``;
 const Hashtags = styled.div``;
 const Button = styled.button``;
+const Preview = styled.div``;
 
 const Form = () => {
     const [postTitle, setPostTitle] = useState("");
@@ -29,6 +31,10 @@ const Form = () => {
         { value: "겨울", label: "겨울" }
     ];
 
+    const getEmbedLink = (link) => {
+        const videoId = getYoutubeKey(link);
+        return `https://www.youtube.com/embed/${videoId}?loop=1&autoplay=1&mute=1&playlist=${videoId}`;
+    };
     const handlePostTitleChange = (event) => {
         setPostTitle(event.target.value);
     };
@@ -63,6 +69,21 @@ const Form = () => {
                 <label>제목:</label>
                 <input placeholder="제목을 입력해주세요." value={postTitle} onChange={handlePostTitleChange} />
             </PostTitle>
+            <div>
+                {youtubeLink && (
+                    <Preview>
+                        <iframe
+                            width="560"
+                            height="315"
+                            src={getEmbedLink(youtubeLink)}
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                            title="YouTube Video Preview"
+                        ></iframe>
+                    </Preview>
+                )}
+            </div>
             <SongTitle>
                 <label>노래제목:</label>
                 <input placeholder="노래 제목을 입력해주세요." value={title} onChange={handleTitleChange} />
