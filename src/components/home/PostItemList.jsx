d8656e4dc1c08ae3236f190d19e2364a59e762c3
escape-engineering <tkdrnr1215@naver.com>
2024-09-03 00:35:27 +0900

    style: 홈페이지 스타일 수정

--- src/components/home/PostItemList.jsx
+++ src/components/home/PostItemList.jsx
@@ -1,12 +1,14 @@
 import PostItem from "./PostItem";
 import styled from "styled-components";
-import useMusicContext from "../../hooks/useMusicContext";
+import { useState } from "react";
 
 const PostItemList = ({ songs, title }) => {
+    const [isListOpen, setIsListOpen] = useState(false);
+
     return (
         <PostItemWrapper>
             <PostWrapTitle>{title}</PostWrapTitle>
-            <PostItemsDiv>
+            <PostItemsDiv $isListOpen={isListOpen}>
                 {songs.length ? (
                     songs.map((music) => {
                         return <PostItem key={music.id} music={music} />;
@@ -15,6 +17,9 @@ const PostItemList = ({ songs, title }) => {
                     <p>어울리는 음악이 없습니다! 해당 계절에 어울리는 음악을 추가해주세요!</p>
                 )}
             </PostItemsDiv>
+            {songs.length > 4 ? (
+                <ListOpenButton onClick={() => setIsListOpen(!isListOpen)}>{isListOpen ? "▲" : "▼"}</ListOpenButton>
+            ) : null}
         </PostItemWrapper>
     );
 };
@@ -22,6 +27,8 @@ const PostItemList = ({ songs, title }) => {
 export default PostItemList;
 
 const PostItemWrapper = styled.ul`
+    background-color: #e5f3fc;
+    border-radius: 20px;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
@@ -29,26 +36,44 @@ const PostItemWrapper = styled.ul`
     margin: 10px 0px;
     box-sizing: border-box;
     padding: 20px 30px;
-    background-color: #d4eaf7;
-    border: 3px solid red;
 `;
 
 const PostWrapTitle = styled.h3`
     border-bottom: 1px solid black;
     width: 100%;
-    padding: 10px 5px;
-    margin-bottom: 40px;
+    padding: 10px 5px 20px 5px;
+    margin-bottom: 20px;
     font-size: 28px;
     font-weight: bold;
 `;
 
 const PostItemsDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    justify-items: center;
     gap: 20px;
     width: 100%;
+    overflow-y: hidden;
     overflow-x: auto;
     padding-bottom: 10px;
+    max-height: ${({ $isListOpen }) => ($isListOpen ? "3000px" : "500px")};
+    transition: max-height 0.5s ease-in-out;
+    height: auto;
+    padding-top: 20px;
+`;
+
+const ListOpenButton = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+    font-weight: bold;
+    font-size: 20px;
+    border-radius: 10px;
+    padding: 3px 0px;
+    cursor: pointer;
+    &:hover {
+        background-color: #cbe2ee;
+    }
 `;
