12e4095c3e88e3aa3c695af529ba4e3209d0fa7a
stopbrother <stopbro0323@naver.com>
2024-09-03 16:24:51 +0900

    style: 게시물 카드

--- src/components/profile/LikedItem.jsx
+++ src/components/profile/LikedItem.jsx
@@ -1,19 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import { getYoutubeKey } from "../../utils";
 import { Item, ItemImgBox, ItemTxtBox } from "./CreatedItem";
+import * as Style from "../home/HomeStyles";
 
 export const LikedItem = ({ likePost }) => {
     const thumbnailKey = getYoutubeKey(likePost.STARTIFY_DATA.url);
     const navigate = useNavigate();
     return (
-        <Item onClick={() => navigate(`/detail?id=${likePost.STARTIFY_DATA.id}`)}>
-            <ItemImgBox>
-                <img src={`https://img.youtube.com/vi/${thumbnailKey}/0.jpg`} alt="" />
-            </ItemImgBox>
+        <Style.ItemLi onClick={() => navigate(`/detail?id=${likePost.STARTIFY_DATA.id}`)}>
+            <Style.TextName>{likePost.STARTIFY_DATA.postTitle}</Style.TextName>
+            <Style.ThumbnailWrap>
+                <ItemImgBox>
+                    <Style.ThumbnailImg
+                        src={`https://img.youtube.com/vi/${thumbnailKey}/0.jpg`}
+                        alt={likePost.STARTIFY_DATA.postTitle}
+                    />
+                </ItemImgBox>
+            </Style.ThumbnailWrap>
             <ItemTxtBox>
                 <span>{likePost.STARTIFY_DATA.STARTIFY_USER.userName}</span>
-                <h1>{likePost.STARTIFY_DATA.postTitle}</h1>
             </ItemTxtBox>
-        </Item>
+            <Style.Text>
+                {likePost.STARTIFY_DATA.name} - {likePost.STARTIFY_DATA.title}
+            </Style.Text>
+        </Style.ItemLi>
     );
 };
