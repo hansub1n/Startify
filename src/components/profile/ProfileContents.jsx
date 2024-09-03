bd421498381c712fb309d85eb4e6381bc25ada83
stopbrother <stopbro0323@naver.com>
2024-09-03 12:10:18 +0900

    update : 프로필 경로 쿼리스트링으로 교체

--- src/components/profile/ProfileContents.jsx
+++ src/components/profile/ProfileContents.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import Intro from "./Intro";
 import Created from "./Created";
 import Liked from "./Liked";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useParams, useSearchParams } from "react-router-dom";
 
-export const ProfileContents = ({ account }) => {
+export const ProfileContents = ({ account, paramView }) => {
+    console.log("paramView", paramView);
     return (
-        <Routes>
-            {/* path는 사용하고 싶은 주소
-            element는 이동했을때 보여주고자 하는 컴포넌트 */}
-            <Route path="/" element={<Intro account={account} />} />
-            <Route path="created" element={<Created />} />
-            <Route path="liked" element={<Liked />} />
-        </Routes>
+        // <Routes>
+        //     {/* path는 사용하고 싶은 주소
+        //     element는 이동했을때 보여주고자 하는 컴포넌트 */}
+        //     <Route path="/" element={<Intro account={account} />} />
+        //     <Route path="created" element={paramView === "created" ? <Created /> : null} />
+        //     <Route path="liked" element={paramView === "liked" ? <Liked /> : null} />
+        // </Routes>
+        <div>
+            {paramView === null && <Intro account={account} />}
+            {paramView === "created" && <Created />}
+            {paramView === "liked" && <Liked />}
+        </div>
     );
 };
