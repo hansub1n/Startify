f1de2b9bd553cb0c1b2461e1badf20993a637aaa
escape-engineering <tkdrnr1215@naver.com>
2024-09-03 14:26:51 +0900

    style: home, search 페이지 styled-component코드 분리

--- src/components/home/PostItem.jsx
+++ src/components/home/PostItem.jsx
@@ -1,31 +1,28 @@
-import React, { useContext, useState } from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
 import { getYoutubeKey } from "../../utils";
-import { UserContext } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import Button from "../common/Button";
 import playButton from "../../assets/playButton.png";
 import likeImg from "../../assets/like.png";
+import * as Style from "./HomeStyles";
 
 const PostItem = ({ music }) => {
     const [isVideoPlayed, setIsVideoPlayed] = useState(false);
     const { postTitle, name, title, url, id } = music;
     const navigate = useNavigate();
-    const { user } = useContext(UserContext);
-    const userId = user?.id;
     const thumbnailKey = getYoutubeKey(url);
     const likeCount = music.likes.length;
 
     return (
-        <ItemLi onClick={() => navigate(`/detail?id=${id}`)}>
-            <TextName>{postTitle}</TextName>
-            <ThumbnailWrap>
-                <ImgIframeWrap>
-                    <ThumbnailImg
+        <Style.ItemLi onClick={() => navigate(`/detail?id=${id}`)}>
+            <Style.TextName>{postTitle}</Style.TextName>
+            <Style.ThumbnailWrap>
+                <Style.ImgIframeWrap>
+                    <Style.ThumbnailImg
                         $isVideoPlayed={isVideoPlayed}
                         src={`https://img.youtube.com/vi/${thumbnailKey}/0.jpg`}
                     />
-                    <VideoIframe
+                    <Style.VideoIframe
                         $isVideoPlayed={isVideoPlayed}
                         width="300"
                         height="224"
@@ -35,16 +32,16 @@ const PostItem = ({ music }) => {
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;"
                         referrerPolicy="strict-origin-when-cross-origin"
                         allowFullScreen
-                    ></VideoIframe>
-                </ImgIframeWrap>
-                <ThumbnailTextWrap $isVideoPlayed={isVideoPlayed}>
-                    <LikesText>{likeCount}</LikesText>
-                    <LikeBtmImg src={likeImg} />
-                </ThumbnailTextWrap>
-            </ThumbnailWrap>
-            <Text>
+                    ></Style.VideoIframe>
+                </Style.ImgIframeWrap>
+                <Style.ThumbnailTextWrap $isVideoPlayed={isVideoPlayed}>
+                    <Style.LikesText>{likeCount}</Style.LikesText>
+                    <Style.LikeBtmImg src={likeImg} />
+                </Style.ThumbnailTextWrap>
+            </Style.ThumbnailWrap>
+            <Style.Text>
                 {name} - {title}
-            </Text>
+            </Style.Text>
             <Button
                 onClick={(e) => {
                     e.stopPropagation();
@@ -57,98 +54,8 @@ const PostItem = ({ music }) => {
             >
                 <img src={playButton} />
             </Button>
-        </ItemLi>
+        </Style.ItemLi>
     );
 };
 
 export default PostItem;
-
-const ItemLi = styled.li`
-    font-family: "SUITE-Regular";
-    cursor: pointer;
-    background-color: white;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    width: 360px;
-    height: 400px;
-    border-radius: 30px;
-    padding: 20px;
-    transition: transform 0.2s, box-shadow 0.2s;
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 5px 5px;
-    h3 {
-        font-family: "GmarketSansMedium";
-    }
-    &:hover {
-        transform: translateY(-10px);
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    }
-`;
-
-const ThumbnailWrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-`;
-
-const ImgIframeWrap = styled.div`
-    width: 300px;
-    height: 224px;
-`;
-
-const ThumbnailImg = styled.img`
-    display: ${({ $isVideoPlayed }) => ($isVideoPlayed ? "none" : "block")};
-    width: 300px;
-    height: 224px;
-    object-fit: fill;
-    border-radius: 20px;
-`;
-
-const VideoIframe = styled.iframe`
-    display: ${({ $isVideoPlayed }) => ($isVideoPlayed ? "block" : "none")};
-    width: 300px;
-    height: 224px;
-    border-radius: 20px;
-`;
-
-const ThumbnailTextWrap = styled.div`
-    display: ${({ $isVideoPlayed }) => ($isVideoPlayed ? "none" : "flex")};
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 3px;
-    position: absolute;
-    bottom: 0px;
-    right: 0px;
-    padding: 5px;
-    background-color: rgb(255, 255, 255, 0.4);
-    border-radius: 10px;
-`;
-
-const LikesText = styled.p`
-    /* color: white; */
-    font-weight: 700;
-`;
-
-const LikeBtmImg = styled.img`
-    width: 40px;
-    height: 40px;
-`;
-
-const TextName = styled.p`
-    font-family: "SUITE-Regular";
-    font-size: 25px;
-    font-weight: 700;
-    margin-bottom: 10px;
-`;
-
-const Text = styled.p`
-    font-family: "SUITE-Regular";
-    font-size: 18px;
-    margin-top: 10px;
-    margin-bottom: 5px;
-`;
--- src/components/home/PostItemList.jsx
+++ src/components/home/PostItemList.jsx
@@ -1,6 +1,6 @@
 import PostItem from "./PostItem";
-import styled from "styled-components";
 import { useEffect, useState } from "react";
+import * as Style from "./HomeStyles";
 
 const PostItemList = ({ songs, title, index }) => {
     const [isListOpen, setIsListOpen] = useState(false);
@@ -35,9 +35,9 @@ const PostItemList = ({ songs, title, index }) => {
     });
 
     return (
-        <PostItemWrapper $index={index}>
-            <PostWrapTitle>{title}</PostWrapTitle>
-            <PostItemsDiv $isListOpen={isListOpen}>
+        <Style.PostItemWrapper $index={index}>
+            <Style.PostWrapTitle>{title}</Style.PostWrapTitle>
+            <Style.PostItemsDiv $isListOpen={isListOpen}>
                 {songs.length ? (
                     songs.map((music) => {
                         return <PostItem key={music.id} music={music} />;
@@ -45,80 +45,18 @@ const PostItemList = ({ songs, title, index }) => {
                 ) : (
                     <p>어울리는 음악이 없습니다! 해당 계절에 어울리는 음악을 추가해주세요!</p>
                 )}
-            </PostItemsDiv>
+            </Style.PostItemsDiv>
             {songs.length > visibleCount ? (
-                <ListOpenButton $index={index} $isListOpen={isListOpen} onClick={() => setIsListOpen(!isListOpen)}>
+                <Style.ListOpenButton
+                    $index={index}
+                    $isListOpen={isListOpen}
+                    onClick={() => setIsListOpen(!isListOpen)}
+                >
                     {isListOpen ? "▲" : "▼"}
-                </ListOpenButton>
+                </Style.ListOpenButton>
             ) : null}
-        </PostItemWrapper>
+        </Style.PostItemWrapper>
     );
 };
 
 export default PostItemList;
-
-const PostItemWrapper = styled.ul`
-    background-color: ${({ $index }) => ($index % 2 == 0 ? "#e5f3fc" : "#6fabda")};
-    border-radius: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    margin: 10px 0px;
-    box-sizing: border-box;
-    padding: 20px 30px;
-`;
-
-const PostWrapTitle = styled.h3`
-    font-family: "SUITE-Regular";
-    border-bottom: 1px solid black;
-    width: 100%;
-    padding: 10px 5px 20px 5px;
-    margin-bottom: 20px;
-    font-size: 28px;
-    font-weight: bold;
-`;
-
-const PostItemsDiv = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    justify-items: center;
-    gap: 20px;
-    width: 100%;
-    overflow-y: hidden;
-    overflow-x: auto;
-    max-height: ${({ $isListOpen }) => ($isListOpen ? "5000px" : "500px")};
-    transition: max-height ${({ $isListOpen }) => ($isListOpen ? "2s ease-in-out" : "1s ease-in-out")};
-    height: auto;
-    padding: 20px 10px;
-
-    @media all and (min-width: 1200px) and (max-width: 1659px) {
-        grid-template-columns: repeat(3, 1fr);
-    }
-
-    @media all and (min-width: 840px) and (max-width: 1199px) {
-        grid-template-columns: repeat(2, 1fr);
-    }
-
-    @media all and (max-width: 839px) {
-        grid-template-columns: repeat(1, 1fr);
-    }
-`;
-
-const ListOpenButton = styled.div`
-    background-color: ${({ $index }) => ($index % 2 == 0 ? "#e5f3fc" : "#6fabda")};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    width: 100%;
-    font-weight: bold;
-    font-size: 20px;
-    border-radius: 10px;
-    padding: 3px 0px;
-    margin-top: 10px;
-    cursor: pointer;
-    &:hover {
-        filter: brightness(0.95);
-    }
-`;
