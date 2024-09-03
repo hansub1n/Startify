2c220533b7221f2b7b3e7c9a1f974a28c64dd37f
escape-engineering <tkdrnr1215@naver.com>
2024-09-03 17:45:58 +0900

    refactor: layout폴더명 변경

--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import styled from "styled-components";
 import LayoutHeader from "../components/layout/LayoutHeader";
 
 const Layout = () => {
@@ -8,16 +7,8 @@ const Layout = () => {
         <>
             <LayoutHeader />
             <Outlet />
-            {/* <LayoutFooter>@copyright. 리액트도14경</LayoutFooter> */}
         </>
     );
 };
 
 export default Layout;
-
-const LayoutFooter = styled.footer`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-`;
