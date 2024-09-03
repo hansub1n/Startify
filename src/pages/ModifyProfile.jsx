788de72b6147672ed91773c60b90c53d742b626a
star1024cd <1star1024cd@gmail.com>
2024-09-03 09:54:04 +0900

    refactor : 프로필 수정 스타일 컴포넌트 분리

--- src/pages/ModifyProfile.jsx
+++ src/pages/ModifyProfile.jsx
@@ -5,101 +5,13 @@ import styled from "styled-components";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
-
-const SignUpContainer = styled.div`
-    max-width: 600px;
-    margin: 30px auto;
-    padding: 20px;
-    background-color: white;
-    border-radius: 10px;
-    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-    text-align: center;
-
-    h2 {
-        margin-bottom: 20px;
-        font-size: 30px;
-        color: #3b3c3d;
-    }
-
-    button {
-        padding: 10px 20px;
-        background-color: #3b3c3d;
-        color: white;
-        border: none;
-        border-radius: 4px;
-        font-size: 16px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-    }
-
-    button:hover {
-        background-color: #00668c;
-    }
-`;
-
-const ImgUploadContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    input {
-        padding: 10px;
-        margin-bottom: 15px;
-    }
-
-    p {
-        color: #3b3c3d;
-        font-size: 14px;
-    }
-`;
-
-const PhotoContainer = styled.div`
-    width: 100px;
-    height: 100px;
-    border-radius: 20%;
-    overflow: hidden;
-    background-color: #d9d9d9;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 10px;
-
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-`;
-
-const InputContainer = styled.div`
-    width: 100%;
-
-    label {
-        font-weight: bold;
-        color: #3b3c3d;
-        margin-bottom: 20px;
-    }
-
-    input {
-        width: 100%;
-        padding: 10px;
-        border: 1px solid #ccc;
-        margin-top: 10px;
-        margin-bottom: 20px;
-        border-radius: 4px;
-        font-size: 14px;
-        color: #3b3c3d;
-        box-sizing: border-box;
-    }
-
-    p {
-        color: #e74c3c;
-        font-size: 20px;
-        margin-top: 5px;
-
-        text-align: center;
-    }
-`;
+import {
+    ButtonContainer,
+    ImgUploadContainer,
+    InputContainer,
+    PhotoContainer,
+    SignUpContainer
+} from "../components/userInfo/UserStyle";
 
 const ModifyProfile = () => {
     const { user } = useContext(UserContext);
@@ -237,7 +149,9 @@ const ModifyProfile = () => {
                         ref={passwordRef}
                     />
                 </InputContainer>
-                <button type="submit">프로필 수정</button>
+                <ButtonContainer>
+                    <button type="submit">프로필 수정</button>
+                </ButtonContainer>
             </form>
         </SignUpContainer>
     );
